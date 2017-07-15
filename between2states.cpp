3a792cff477552f2d550bd136c2372b8f437fc36
Abhinav Utkarsh <abutkarsh8018@gmail.com>
2017-07-15 19:03:50 +0530

    Create between2states.cpp

---
+++ between2states.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getTotalX(vector < int > a, vector < int > b){
+    // Complete this function
+}
+
+int main() {
+    int n;
+    int m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for(int a_i = 0; a_i < n; a_i++){
+       cin >> a[a_i];
+    }
+    vector<int> b(m);
+    for(int b_i = 0; b_i < m; b_i++){
+       cin >> b[b_i];
+    }
+    int total = getTotalX(a, b);
+    cout << total << endl;
+    return 0;
+}
